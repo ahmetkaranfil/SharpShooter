ee933d9b34b93281d6badd62474832488a020e95
Ahmet <ahmtbjk-21@hotmail.com>
2025-12-03 17:07:21 +0300

    İlk olarak kendimize bir düşman yarattık ve NavMesh Surface oluşturduk. Bunu oluşturmamızın nedeni zeminin düşmanlarımız engelleri aşıp kovalama fonksiyonunu gerçekleştirebilecek hale getirilmeye çalışılmasıydı. Ayrıca Düşman.cs adında ilk C# scriptimizi oluşturduk. Bu scriptin temel amacı oluşturduğumuz düşmanın oyuncumuzu kovalamasıydı. Bunun dışında materyaller oluşturarak oyuna şimdilik renk kattık.

---
+++ Assets/Scripts/Düşman.cs
@@ -0,0 +1,26 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Düşman : MonoBehaviour
+{
+    FirstPersonController player;
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = FindFirstObjectByType<FirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+
+    }
+}
