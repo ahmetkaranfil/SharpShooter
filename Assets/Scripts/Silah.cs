0da16d6a48c2ea616e41fbc6e3e2ea349038c686
Ahmet <ahmtbjk-21@hotmail.com>
2025-12-12 15:28:59 +0300

    Oyunumuzda mermilerin sahnedeki toplanabilir silahların içinden geçmediğini fark ettik. Bu sorunu çözmek için internette çeşitli araştırmalar yaptık ve ardından Silah scriptimizde yer alan Raycast fonksiyonunun parametrelerinde bazı düzenlemelere gittik. İlk olarak, üst kısımda bir LayerMask interactionLayers değişkeni oluşturup SerializeField ile inspector üzerinden ayarlanabilir hale getirdik. Daha sonra bu katman filtresini Raycast fonksiyonuna ekledik. Ancak bu değişiklikten sonra mermilerin düşmanların içinden geçmeye başladığını fark ettik. Bunu düzeltmek için Raycast işlemine QueryTriggerInteraction.Ignore parametresini ekledik. Böylece toplanabilir nesnelerle doğru şekilde etkileşim sağlanırken, düşmanlarla olan çarpışmalar da düzgün çalışmaya devam etti.

--- Assets/Scripts/Silah.cs
+++ Assets/Scripts/Silah.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class Silah : MonoBehaviour
 {
     [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] LayerMask interactionLayers;
 
     public void Shoot(SilahSO silahSO)
     {
         RaycastHit hit;
         muzzleFlash.Play();
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, interactionLayers, QueryTriggerInteraction.Ignore))
         {
             Instantiate(silahSO.HitVFXPrefab, hit.point, Quaternion.identity);
             DüşmanSağlığı enemyHealth = hit.collider.GetComponent<DüşmanSağlığı>();
