caac175c0c1e4f5f182d787c6d6ec2fc1a5bd6c9
Ahmet <ahmtbjk-21@hotmail.com>
2025-12-14 17:19:52 +0300

    İndirdiğimiz assetler arasından bir kalkan simgesi ve bar simgeleri belirledik. Öncelikle UI -> Image kullanarak kalkan simgesini oyun arayüzüne yerleştirdik. Ardından bir Grid Layout oluşturarak, bunun altına tekrar UI -> Image nesneleri ekleyip kalkan barlarını tasarladık. Bu yapı sayesinde barlar yatay ve düzenli bir şekilde sıralandı; aralarındaki boşlukları ve kalınlıklarını ihtiyacımıza göre ayarladık. Daha sonra "OyuncuSağlığı" scriptine geçerek [SerializeField] Image[] shieldBars adlı bir dizi ekledik. Kalkan barlarının güncellenmesini sağlamak için AdjustShieldBar() adlı bir fonksiyon oluşturduk. Bu fonksiyon oyuncu hasar aldığında alınan hasar miktarına bağlı olarak kalkan barlarının azalmasını sağladı. Son olarak hasar alındığında barların da güncellenebilmesi için bu fonksiyonu TakeDamage() fonksiyonu içerisine dahil ettik. Gerekli düzenlemelerin ardından oyunumuzu test ederek sistemin doğru şekilde çalıştığını doğruladık.

--- Assets/Scripts/OyuncuSağlığı.cs
+++ Assets/Scripts/OyuncuSağlığı.cs
@@ -1,11 +1,13 @@
 using Cinemachine;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class OyuncuSağlığı : MonoBehaviour
 {
+    [Range(1, 10)]
     [SerializeField] int startingHealth = 5;
     [SerializeField] CinemachineVirtualCamera deathVirtualCamera;
     [SerializeField] Transform weaponCamera;
+    [SerializeField] Image[] shieldBars;
 
     int currentHealth;
     int gameOverVirtualCameraPriority = 20;
@@ -13,11 +15,14 @@ public class OyuncuSağlığı : MonoBehaviour
     void Awake()
     {
         currentHealth = startingHealth;
+        AdjustShieldBar();
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        AdjustShieldBar();
+
         Debug.Log(amount + " hasar alındı");
 
         if(currentHealth <= 0)
@@ -25,6 +30,21 @@ public class OyuncuSağlığı : MonoBehaviour
             weaponCamera.parent = null;
             deathVirtualCamera.Priority = gameOverVirtualCameraPriority;
             Destroy(this.gameObject);
+        }
+    }
+
+    void AdjustShieldBar()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
         }
     }
 }
