1778176c9fcf1e866cdf528cb56f20f24feea8bb
Ahmet <ahmtbjk-21@hotmail.com>
2025-12-18 22:48:55 +0300

    Oyun sahnemize bir düşman sayacı ekledik ve eğer düşman sayacımızı sıfırlayabilirsek kazanadığımızı gösteren bir UI ekledik.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,28 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text enemiesLeftText;
+    [SerializeField] GameObject winPanel;
+
+    int enemiesLeft = 0;
+
+    const string ENEMIES_LEFT_STRING = "Kalan Düşman: ";
+
+    public void UpdateEnemiesLeft(int amount = 0)
+    {
+        enemiesLeft += amount;
+        enemiesLeftText.text = ENEMIES_LEFT_STRING + enemiesLeft.ToString();
+
+        if (enemiesLeft <= 0)
+        {
+            winPanel.SetActive(true);
+        }
+    }
+
+
     public void RestartButton()
     {
         int currentLevel = SceneManager.GetActiveScene().buildIndex;
