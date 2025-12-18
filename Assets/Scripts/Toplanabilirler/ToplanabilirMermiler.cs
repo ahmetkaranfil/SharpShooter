047206191e29b859888a43a8ddcf166a98181095
Halil Cuneyt <halilcavuss21@gmail.com>
2025-12-18 12:26:49 +0300

    Düzenlemeler yapıldı.

--- Assets/Scripts/Toplanabilirler/ToplanabilirMermiler.cs
+++ Assets/Scripts/Toplanabilirler/ToplanabilirMermiler.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ToplanabilirMermiler : Toplanabilirler
 {
-    [SerializeField] int mermiSayisi = 100;
+    [SerializeField] int mermiSayisi = 90;
     protected override void OnPickUp(ActiveWeapon activeWeapon)
     {
         activeWeapon.AdjustAmmo(mermiSayisi);
