9d96579cd644a27f45c7c7ed4a6aa1ab14aefd64
Ahmet <ahmtbjk-21@hotmail.com>
2025-12-20 17:53:53 +0300

    MainMenu'den oyunumuza geçmek için gerekli kodlar yazıldı.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void GoToScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
